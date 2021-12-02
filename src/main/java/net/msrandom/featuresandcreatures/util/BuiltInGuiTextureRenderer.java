cf03879fd34f9217b8b44984a82ed0e930ff9efa
Ashley Wright <ahmedtrg981106@gmail.com>
2021-12-02 03:47:26 +0300

    Add a small API for allowing items to have a texture on the gui, like the trident

---
+++ src/main/java/net/msrandom/featuresandcreatures/util/BuiltInGuiTextureRenderer.java
@@ -0,0 +1,29 @@
+package net.msrandom.featuresandcreatures.util;
+
+import net.minecraft.client.renderer.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BuiltInGuiTextureRenderer {
+    private static final Map<Item, ModelResourceLocation> MODELS = new HashMap<>();
+
+    public static void register(Item item) {
+        MODELS.put(item, null);
+    }
+
+    public static ModelResourceLocation getItemModel(ItemStack stack) {
+        Item item = stack.getItem();
+        if (!MODELS.containsKey(item)) return null;
+        if (item.getRegistryName() == null) return null; // Not registered, so we don't care.
+        return MODELS.compute(item, (k, v) -> {
+            if (v == null) {
+                return new ModelResourceLocation(k.getRegistryName(), "");
+            } else {
+                return v;
+            }
+        });
+    }
+}
