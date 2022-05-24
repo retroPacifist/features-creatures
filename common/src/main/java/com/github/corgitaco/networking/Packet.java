8c91cddfb62d3fb7c167c6a27ae89b63d839cd15
chaottic <64570529+chaottic@users.noreply.github.com>
2022-05-24 19:58:28 +1000

    Wip networking handler rewrite.

---
+++ common/src/main/java/com/github/corgitaco/networking/Packet.java
@@ -0,0 +1,29 @@
+package com.github.corgitaco.networking;
+
+import lombok.Getter;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public interface Packet {
+
+    void handle(Level level, Player player);
+
+    abstract class Factory<T extends Packet> implements Serializer<T>, Deserializer<T> {
+        @Getter
+        private final Class<T> clazz;
+
+        public Factory(Class<T> clazz) {
+            this.clazz = clazz;
+        }
+    }
+
+    interface Serializer<T extends Packet> extends BiConsumer<T, FriendlyByteBuf> {
+    }
+
+    interface Deserializer<T extends Packet> extends Function<FriendlyByteBuf, T> {
+    }
+}
